54fe9a58b9b81cebb92bffb00b312295f9b8bf3f
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2019-10-17 08:44:58 -0600

    update dependencies

--- dll/renderer.dev.dll.js
+++ dll/renderer.dev.dll.js
@@ -135,6 +135,18 @@ eval("module.exports = bootstrap;\n\n//# sourceURL=webpack://renderer/external_%
 
 /***/ }),
 
+/***/ "core-js":
+/*!**************************!*\
+  !*** external "core-js" ***!
+  \**************************/
+/*! no static exports found */
+/*! all exports used */
+/***/ (function(module, exports) {
+
+eval("module.exports = core-js;\n\n//# sourceURL=webpack://renderer/external_%22core-js%22?");
+
+/***/ }),
+
 /***/ "devtron":
 /*!**************************!*\
   !*** external "devtron" ***!
